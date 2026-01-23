32d03b88183e3a248686ff705fef5c3c6d0e2ad1
jpcshka <art_filat@mail.ru>
2026-01-23 21:23:23 +0300

    add config.py

--- app/core/config.py
+++ app/core/config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-REFRESH_TOKEN=os.getenv("REFRESH_TOKEN")
-SPOTIFY_CLIENT_ID=os.getenv("SPOTIFY_CLIENT_ID")
-SPOTIFY_CLIENT_SECRET=os.getenv("SPOTIFY_CLIENT_SECRET")
-DB_PATH=os.getenv("DB_PATH")
+REFRESH_TOKEN = os.getenv("REFRESH_TOKEN")
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+DB_PATH = os.getenv("DB_PATH")
 
 if not all([REFRESH_TOKEN, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, DB_PATH]):
     raise ValueError("Проверьте, что все переменные окружения заданы в .env файле")
